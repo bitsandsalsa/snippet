abd2f307eb2a61b8f1223a33900f243f816ad313
bitsandsalsa <bitsandsalsa@users.noreply.github.com>
2017-05-15 16:50:03 -0700

    handle quit and threading

--- tkinter.py
+++ tkinter.py
@@ -1,3 +1,6 @@
+import threading
+import Tkinter as tk
+import tkMessageBox
 import ttk
 
 # http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/index.html
@@ -14,25 +17,30 @@ sty.theme_use()
 sty.theme_use('classic')
 
 
+class LifetimeError(Exception):
+    pass
+
 # Example Tkinter app. A progress bar.
 class GuiProgressBar(ttk.Frame):
     def __init__(self, title, work_count, work_func, *func_args):
         ttk.Frame.__init__(self, relief='ridge', borderwidth=2)
         self.work_count = work_count
-        self.work_func = work_func
-        self.func_args = func_args
+        self.worker_task = threading.Thread(target=work_func, args=func_args)
+        self.is_pending_quit = False
         self.master.title(title)
         self.pack(fill='both', expand=1)  # fill both x and y directions, expand with window changes
         self._create_widgets()
 
     def _create_widgets(self):
-        self.my_button = ttk.Button(self, text='Start', command=self._start)
+        self.my_button_text = tk.StringVar(value='Start')
+        self.my_button = ttk.Button(self, textvariable=self.my_button_text, command=self._start)
         self.my_button.pack()
 
         self.my_label_frame = ttk.LabelFrame(self, text='Work Items')
         self.my_label_frame.pack(fill='x')
 
-        self.my_label = ttk.Label(self.my_label_frame, anchor='w')
+        self.my_label_text = tk.StringVar()
+        self.my_label = ttk.Label(self.my_label_frame, anchor='w', textvariable=self.my_label_text)
         self.my_label.pack(fill='x')
 
         self.my_progress_bar = ttk.Progressbar(
@@ -44,29 +52,38 @@ class GuiProgressBar(ttk.Frame):
         )
         self.my_progress_bar.pack(fill='both')
 
-        self.my_status_label = ttk.Label(self, anchor='w', text='0 / {}'.format(self.work_count))
+        self.my_status_label_text = tk.Stringvar(value='0 / {}'.format(self.work_count))
+        self.my_status_label = ttk.Label(self, anchor='w', textvariable=self.my_status_label_text)
         self.my_status_label.pack(fill='x')
+
+    def _confirm_quit(self):
+        if tkMessageBox.askyesno(message='Quit?'):
+            self.is_pending_quit = True
+            self.master.destroy()
 
     def _start(self):
         self.my_button.state(['disabled'])
-        self.my_button['text'] = 'Running...'
-        # Schedule a thread off of mainloop. Seems like only one worker thread because workers
-        # execute in order and block on each other.
-        self.after(1, self.work_func, *self.func_args)
+        self.my_button_text.set('Running...')
+        # Schedule a new thread. Can't use "after" function because a long running task would stall
+        # the GUI mainloop and GUI would become unresponsive. The "after" function seems to add
+        # tasks to an ordered work queue where one task runs at a time.
+        self.worker_task.start()
 
     def update_progress(self, delta):
+        if self.is_pending_quit:
+            raise LifetimeError('User initiated quit.')
         self.my_progress_bar.step(delta)
-        self.my_status_label['text'] = '{} / {}'.format(
+        self.my_status_label_text.set('{} / {}'.format(
             int(self.my_progress_bar['value']) + delta,
             self.work_count
-        )
-        self.update()
+        ))
 
     def update_work_item_text(self, work_itme):
-        self.my_label['text'] = work_item
+        self.my_label_text.set(work_item)
 
     def finish_work(self):
-        self.my_button['text'] = 'Finished'
+        self.my_button.state(['disabled'])
+        self.my_button_text.set('Finished')
 
 class Worker(object):
     def __init__(self):
@@ -78,10 +95,13 @@ class Worker(object):
         self.progress_bar.mainloop()
 
     def do_work(self):
-        for item in self.work_items:
-            self.progress_bar.update_work_item_text(str(item))
+        try:
+            for item in self.work_items:
+                self.progress_bar.update_work_item_text(str(item))
 
-            # calculations
+                # calculations
 
-            self.progress_bar.update_progress(1)
-        self.progress_bar.finish_work()
+                self.progress_bar.update_progress(1)
+            self.progress_bar.finish_work()
+        except LifetimeError as e:
+            print e
